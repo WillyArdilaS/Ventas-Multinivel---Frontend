318d683352083e6e0c64685e8cf77062a7fcc8bf
William Ardila <williamardilasanchez@gmail.com>
2023-03-18 17:29:10 -0500

    First interface redirection

--- src/components/client_user/SignUp.jsx
+++ src/components/client_user/SignUp.jsx
@@ -15,10 +15,6 @@ const SignUp = ({gradient, color2}) => {
         console.log("Registro funcionando");
     }
 
-    const handleChangePage = () => {
-        navigate("LogIn");
-    }
-
     return (
         <main className="container flex justify-center mx-auto mt-24">
             <article id="userRegister" className={`w-11/12 2xl:w-1/4 xl:w-2/5 lg:w-1/2 md:w-3/5 sm:w-3/4 absolute rounded-t-2xl rounded-b-xl bg-gradient-to-b ` + gradient}>
@@ -28,53 +24,45 @@ const SignUp = ({gradient, color2}) => {
                     <div className="w-4/5 flex justify-between mx-auto">
                         <div id="form-name">
                             <label htmlFor="name"></label>
-                            <input type="text" name="name" id="name" placeholder="Nombre" className="w-32 lg:w-44 sm:w-44 mb-6 px-3 py-2 rounded-md bg-white text-gray-slate 
-                            font-medium placeholder-slate-500" onChange={(e) => setName(e.target.value)} required/>
+                            <input type="text" name="name" id="name" placeholder="Nombre" className="w-32 lg:w-44 sm:w-44 mb-6 px-3 py-2 rounded-md bg-white shadow-md text-black 
+                            font-medium font-title placeholder-slate-400" onChange={(e) => setName(e.target.value)} required/>
                         </div>
 
                         <div id="form-lastName">
                             <label htmlFor="lastName"></label>
-                            <input type="text" name="lastName" id="lastName" placeholder="Apellido" className="w-32 lg:w-44 sm:w-44 mb-6 px-3 py-2 rounded-md bg-white 
-                            text-slate-800 font-medium placeholder-slate-500" onChange={(e) => setLastName(e.target.value)} required/>
+                            <input type="text" name="lastName" id="lastName" placeholder="Apellido" className="w-32 lg:w-44 sm:w-44 mb-6 px-3 py-2 rounded-md bg-white shadow-md 
+                            text-black font-medium font-title placeholder-slate-400" onChange={(e) => setLastName(e.target.value)} required/>
                         </div>
                     </div>
 
                     <div id="form-username" className="flex justify-center">
                         <label htmlFor="username"></label>
-                        <input type="text" name="username" id="username" placeholder="Nombre de usuario" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white text-slate-800 
-                        font-medium placeholder-slate-500" onChange={(e) => setUsername(e.target.value)} required/>
+                        <input type="text" name="username" id="username" placeholder="Nombre de usuario" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white shadow-md text-black 
+                        font-medium font-title placeholder-slate-400" onChange={(e) => setUsername(e.target.value)} required/>
                     </div>
 
                     <div id="form-email" className="flex justify-center">
                         <label htmlFor="email"></label>
-                        <input type="email" name="email" id="email" placeholder="Correo electrónico" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white text-slate-800 
-                        font-medium placeholder-slate-500" onChange={(e) => setEmail(e.target.value)} required/>
+                        <input type="email" name="email" id="email" placeholder="Correo electrónico" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white shadow-md text-black 
+                        font-medium font-title placeholder-slate-400" onChange={(e) => setEmail(e.target.value)} required/>
                     </div>
                 
                     <div id="form-password" className="flex justify-center">
                         <label htmlFor="password"></label>
-                        <input type="password" name="password" id="password" placeholder="Contraseña" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white text-slate-800 
-                        font-medium placeholder-slate-500" onChange={(e) => setPassword(e.target.value)} required/>
+                        <input type="password" name="password" id="password" placeholder="Contraseña" className="w-4/5 mb-6 px-3 py-2 rounded-md bg-white shadow-md text-black 
+                        font-medium font-title placeholder-slate-400" onChange={(e) => setPassword(e.target.value)} required/>
                     </div>
 
                     <div id="form-confirmPassword" className="flex justify-center">
                         <label htmlFor="confirmPassword"></label>
-                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmar contraseña" className="w-4/5 px-3 py-2 rounded-md bg-white 
-                        text-slate-800 font-medium placeholder-slate-500" onChange={(e) => setPasswordConfirmation(e.target.value)} required/>
+                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmar contraseña" className="w-4/5 px-3 py-2 rounded-md bg-white shadow-md 
+                        text-black font-medium font-title placeholder-slate-400" onChange={(e) => setPasswordConfirmation(e.target.value)} required/>
                     </div>
 
-                    <section className="flex pb-10 mx-16 mt-16">
-                        <div id="form-signUp" className="flex justify-center content-center w-1/2">
-                            <input type="button" id="button-signUp" value="Crear cuenta" onClick={handleCreateUser}
-                            className={`px-5 py-3 border-white border-x-2 border-y-2 rounded-lg bg-white shadow-lg text-${color2} text-sm font-semibold font-title 
-                            hover:cursor-pointer hover:bg-transparent hover:text-white transition-colors`}/>
-                        </div>
-
-                        <div id="form-signIn" className="flex justify-center content-center w-1/2">
-                            <input type="button" id="button-signIn" value="Iniciar Sesión" onClick={handleChangePage} 
-                            className={`px-5 py-3 border-white border-x-2 border-y-2 border-dashed rounded-lg bg-${color2} shadow-lg text-white text-sm font-semibold font-title
-                            hover:cursor-pointer hover:bg-white hover:text-${color2} transition-colors`}/>
-                        </div>
+                    <section className="flex justify-center pb-10 mx-16 mt-16">
+                        <input type="button" id="button-signUp" value="Crear cuenta" onClick={handleCreateUser}
+                        className={`w-1/2 px-5 py-3 border-white border-x-2 border-y-2 rounded-lg bg-white shadow-lg text-${color2} text-sm font-semibold font-title 
+                        hover:cursor-pointer hover:bg-transparent hover:text-white transition-colors`}/>
                     </section>
                 </form>
             </article>
--- src/components/general/SelectUser.jsx
+++ src/components/general/SelectUser.jsx
@@ -1,30 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
 const SelectUser = ({setUser}) => {
-  return (
-    <>
-        <h1 className="mt-20 text-6xl text-center font-title font-semibold animate__animated animate__zoomIn">
-            Bienvenido a <span className="text-darkGreen font-bold"> NatAmE </span>
-        </h1>
+    const navigate = useNavigate();
 
-        <h2 className="mt-8 text-3xl text-center font-subtitle font-medium animate__animated animate__zoomIn" >
-            Elige con que rol quieres ingresar
-        </h2>
+    const handleEnterClient = () => {
+        setUser("cliente")
+        navigate("/LogIn");
+    }
 
-        <div className="flex flex-col justify-center items-center mt-24">
-            <div id="form-buttonSignIn" className="flex justify-center content-center w-1/2">
-                <input type="button" id="button-signIn" value="Cliente" onClick={setUser("cliente")} 
-                className="w-96 h-20 px-5 py-3 border-darkGreen border-x-4 border-y-4 rounded-lg bg-darkGreen shadow-lg text-white text-xl font-bold font-title 
-                hover:cursor-pointer hover:bg-transparent hover:text-darkGreen transition-colors animate__animated animate__infinite animate__pulse"/>
+    const handleEnterSR = () => {
+        setUser("representante")
+        navigate("/LogIn");
+    }
+
+    return (
+        <>
+            <h1 className="mt-20 text-6xl text-center font-title font-semibold animate__animated animate__zoomIn">
+                Bienvenido a <span className="text-darkGreen font-bold"> NatAmE </span>
+            </h1>
+
+            <h2 className="mt-8 text-3xl text-center font-subtitle font-medium animate__animated animate__zoomIn" >
+                Elige con que rol quieres ingresar
+            </h2>
+
+            <div className="flex flex-col justify-center items-center mt-24">
+                <div id="form-buttonSignIn" className="flex justify-center content-center w-1/2">
+                    <input type="button" id="button-signIn" value="Cliente" onClick={handleEnterClient} 
+                    className="w-96 h-20 px-5 py-3 border-darkGreen border-x-4 border-y-4 rounded-lg bg-darkGreen shadow-lg text-white text-xl font-bold font-title 
+                    hover:cursor-pointer hover:bg-transparent hover:text-darkGreen transition-colors animate__animated animate__infinite animate__pulse"/>
+                </div>
+
+                <div id="form-buttonSignIn" className="flex justify-center content-center mt-12">
+                    <input type="button" id="button-signIn" value="Representante de ventas" onClick={handleEnterSR}
+                    className="w-96 h-20 px-5 py-3 border-darkBlue border-x-4 border-y-4 rounded-lg bg-darkBlue shadow-lg text-white text-lg font-bold font-title 
+                    hover:cursor-pointer hover:bg-transparent hover:text-darkBlue transition-colors animate__animated animate__infinite animate__pulse"/>
+                </div>
+        
             </div>
-
-            <div id="form-buttonSignIn" className="flex justify-center content-center mt-12">
-                <input type="button" id="button-signIn" value="Representante de ventas" onClick={setUser("representante")}
-                className="w-96 h-20 px-5 py-3 border-darkBlue border-x-4 border-y-4 rounded-lg bg-darkBlue shadow-lg text-white text-lg font-bold font-title 
-                hover:cursor-pointer hover:bg-transparent hover:text-darkBlue transition-colors animate__animated animate__infinite animate__pulse"/>
-            </div>
-    
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default SelectUser
